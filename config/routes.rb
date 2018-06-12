bd743221a7b8ea9750ea86697fdf865dc4e1d4da
Elsa Szymczak <elsaszymczak@gmail.com>
2018-06-12 14:21:15 +0200

    navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'bookings/new'
+  get 'bookings/create'
+  get 'bookings/edit'
+  get 'bookings/update'
+  get 'bookings/destroy'
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'rooms/new'
+  get 'rooms/create'
+  get 'rooms/edit'
+  get 'rooms/update'
+  get 'rooms/destroy'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
